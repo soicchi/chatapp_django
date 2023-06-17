4b105da1c8c23fe3da14eeff29a6a11b7eab6c8b
soichiro-nishizono <soicchi@icloud.com>
2023-06-18 01:00:39 +0900

    feat: created test for chat/views.py

--- chat/tests/test_views.py
+++ chat/tests/test_views.py
@@ -25,11 +25,20 @@ def test_user():
 
 @pytest.mark.django_db
 def test_leave_room_success(api_client, test_user):
+    # テストユーザー作成
+    User = get_user_model()
+    test_user2 = User.objects.create_user(
+        name="test_user2",
+        email="test2@test.com",
+        password="password",
+    )
+
     # テストルームを作成
     room = Room.objects.create(name="test_room", admin_user=test_user)
 
     # RoomMemberのレコードも追加
     RoomMember.objects.create(user_id=test_user.id, room_id=room.id)
+    RoomMember.objects.create(user_id=test_user2.id, room_id=room.id)
 
     input_data = {
         "user_id": test_user.id,
@@ -45,6 +54,25 @@ def test_leave_room_success(api_client, test_user):
     # チャットルームに該当ユーザーが含まれていないか検証
     room.refresh_from_db()
     assert not test_user in room.users.all()
+
+
+@pytest.mark.django_db
+def test_leave_room_failure(api_client, test_user):
+    # テストルームを作成
+    room = Room.objects.create(name="test_room", admin_user=test_user)
+
+    # RoomMemberのレコードも追加
+    RoomMember.objects.create(user_id=test_user.id, room_id=room.id)
+
+    input_data = {
+        "user_id": "",
+        "room_id": room.id,
+    }
+
+    # ユーザーの認証を通す
+    api_client.force_authenticate(user=test_user)
+    response = api_client.post(reverse("chat:leave_room"), input_data, format="json")
+    assert response.status_code == 400
 
 
 @pytest.mark.django_db
