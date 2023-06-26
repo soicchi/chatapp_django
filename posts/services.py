c69688cba0a12f7bd5b600d55471f27445f8955a
soichiro-nishizono <soicchi@icloud.com>
2023-06-26 20:44:14 +0900

    feat: created services to create post

---
+++ posts/services.py
@@ -0,0 +1,29 @@
+from .models import Post
+from chat_rooms.models import Room
+
+
+class PostManagerService:
+    """Postモデルの基本的な作成、取得、更新、削除を行う"""
+
+    def __init__(self, post_model=Post, room_model=Room) -> None:
+        self.post_model = post_model
+        self.room_model = room_model
+
+    def create_post(self, message: str, user: int, room_id: int) -> Post:
+        """投稿を作成
+
+        Args:
+            message (str): 投稿するテキストメッセージ
+            user (int): 投稿するユーザーインスタンス
+            room_id (int): 投稿するルームID
+
+        Returns:
+            Post: 投稿されたPostモデルのインスタンス
+        """
+
+        room = self.room_model.fetch_room(room_id)
+        post = self.post_model.objects.create(
+            message=message, user=user, room=room
+        )
+
+        return post
---
+++ posts/tests/test_services.py
@@ -0,0 +1,34 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+from chat_rooms.models import Room
+from posts.services import PostManagerService
+
+
+
+@pytest.mark.django_db
+def test_create_post():
+    # テストユーザー作成
+    User = get_user_model()
+    user = User.objects.create_user(
+        name="test_user",
+        email="test@test.com",
+        password="password",
+    )
+
+    # テストルーム作成
+    room = Room.objects.create(
+        name="test_room",
+        admin_user_id=user.id,
+    )
+
+    post_manager = PostManagerService()
+    post = post_manager.create_post(
+        message="テストメッセージ",
+        user=user,
+        room_id=room.id,
+    )
+
+    assert post.message == "テストメッセージ"
+    assert post.user == user
+    assert post.room == room
